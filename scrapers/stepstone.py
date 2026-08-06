b8cb97fd173f748a7e2194aeb52da6a526527c83
Farhad Vaseghi <54831801+farhadvaseghi@users.noreply.github.com>
2026-08-06 17:45:53 +0200

    Refactor imports and add company filter

--- scrapers/stepstone.py
+++ scrapers/stepstone.py
@@ -24,7 +24,13 @@ import requests
 from bs4 import BeautifulSoup
 
 import config
-from scrapers.common import get_logger, make_job, passes_permanent_filter, passes_seniority_filter
+from scrapers.common import (
+    get_logger,
+    make_job,
+    passes_company_filter,
+    passes_permanent_filter,
+    passes_seniority_filter,
+)
 
 log = get_logger("stepstone")
 
@@ -155,6 +161,8 @@ def scrape():
                 continue
             check_text = f"{title} {item.get('company', '')} {item.get('context_text', '')}"
             if not passes_permanent_filter(check_text):
+                continue
+            if not passes_company_filter(item.get("company", "")):
                 continue
 
             age_days = item.get("age_days")
